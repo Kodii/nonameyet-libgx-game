5550e74a51faad12dd2e2c8c65ec8e6237d832a5
ddedek <daniel.dedek@icloud.com>
2020-03-15 19:55:18 +0100

    Add lifes to PlayerHUD

--- core/src/com/nonameyet/ui/PlayerHUD.java
+++ core/src/com/nonameyet/ui/PlayerHUD.java
@@ -1,13 +1,15 @@
 package com.nonameyet.ui;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.nonameyet.environment.AssetName;
@@ -18,17 +20,17 @@ import com.nonameyet.utils.Constants;
 public class PlayerHUD implements Screen {
     private static final String TAG = PlayerHUD.class.getSimpleName();
 
+    private static final String LIFE_FULL = "FULL";
+    private static final String LIFE_EMPTY = "EMPTY";
+
     private Stage _stage;
     private Viewport _viewport;
     private Camera _camera;
-
-    private Table _table;
 
     private TextureRegion _textureLifeEmpty;
     private TextureRegion _textureLifeFull;
 
     private int _hpVal = 3;
-
     private int _hpCurrentMax = 4;
 
     public PlayerHUD(Camera camera) {
@@ -40,7 +42,6 @@ public class PlayerHUD implements Screen {
 
         cameraFrame();
         addLife();
-        renderLife();
     }
 
     private void cameraFrame() {
@@ -62,7 +63,7 @@ public class PlayerHUD implements Screen {
     }
 
     private Image addLifeEmpty() {
-        Image lifeEmpty = new Image();
+        Image lifeEmpty;
         lifeEmpty = new Image(_textureLifeEmpty);
         lifeEmpty.setSize(_textureLifeEmpty.getRegionWidth() * (AbstractScreen.VIEWPORT.physicalWidth / Constants.CAMERA_PIXELS_WIDTH),
                 _textureLifeEmpty.getRegionHeight() * (AbstractScreen.VIEWPORT.physicalHeight / Constants.CAMERA_PIXELS_HEIGHT));
@@ -71,11 +72,10 @@ public class PlayerHUD implements Screen {
     }
 
     private Image addLifeFull() {
-        Image lifeFull = new Image();
+        Image lifeFull;
         lifeFull = new Image(_textureLifeFull);
         lifeFull.setSize(_textureLifeFull.getRegionWidth() * (AbstractScreen.VIEWPORT.physicalWidth / Constants.CAMERA_PIXELS_WIDTH),
                 _textureLifeFull.getRegionHeight() * (AbstractScreen.VIEWPORT.physicalHeight / Constants.CAMERA_PIXELS_HEIGHT));
-
         return lifeFull;
     }
 
@@ -85,28 +85,76 @@ public class PlayerHUD implements Screen {
 
     @Override
     public void render(float delta) {
+        handleInput(delta);
+
+        renderLife();
 
         _stage.act(delta);
         _stage.draw();
     }
 
+    private void handleInput(float delta) {
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.MINUS)) {
+            if (_hpVal > 0) _hpVal -= 1;
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.PLUS)) {
+            if (_hpVal < _hpCurrentMax) _hpVal += 1;
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
+            _hpCurrentMax += 1;
+            _hpVal = _hpCurrentMax;
+        }
+
+    }
+
+    // todo: extract to another class
     private void renderLife() {
+        Array<Actor> actors = _stage.getActors();
 
         float ppi_width = AbstractScreen.VIEWPORT.physicalWidth / Constants.CAMERA_PIXELS_WIDTH;
         float ppi_height = AbstractScreen.VIEWPORT.physicalHeight / Constants.CAMERA_PIXELS_HEIGHT;
-        Gdx.app.log(TAG, "ppi_width: " + ppi_width);
-        Gdx.app.log(TAG, "ppi_height: " + ppi_height);
 
         for (int i = 0; i < _hpCurrentMax; i++) {
             Image lifeFull = addLifeFull();
+            Image lifeEmpty = addLifeEmpty();
 
+            // add full lifes
+            if (i < _hpVal) {
+                positionLife(ppi_width, ppi_height, i, lifeFull, lifeFull.getPrefHeight(), LIFE_FULL);
+                compareLifeActors(actors, lifeFull);
+            }
 
-            lifeFull.setPosition((7 * ppi_width) + i * (lifeFull.getPrefHeight() * ppi_width) + (i > 0 ? (i * (2 * ppi_width)) : 0),
-                    AbstractScreen.VIEWPORT.physicalHeight - (lifeFull.getPrefHeight() * ppi_height) - (7 * ppi_width));
-
-            _stage.addActor(lifeFull);
+            // add empty lifes
+            if (i >= _hpVal && i <= _hpCurrentMax) {
+                positionLife(ppi_width, ppi_height, i, lifeEmpty, lifeFull.getPrefHeight(), LIFE_EMPTY);
+                compareLifeActors(actors, lifeEmpty);
+            }
         }
 
+    }
+
+    // todo: extract to another class
+    private void positionLife(float ppi_width, float ppi_height, int i, Image lifeFull, float prefHeight, String lifeFull2) {
+        lifeFull.setPosition((7 * ppi_width) + i * (prefHeight * ppi_width) + (i > 0 ? (i * (2 * ppi_width)) : 0),
+                AbstractScreen.VIEWPORT.physicalHeight - (prefHeight * ppi_height) - (7 * ppi_width));
+        lifeFull.setName(lifeFull2 + i);
+    }
+
+    // todo: extract to another class
+    private void compareLifeActors(Array<Actor> actors, Image life) {
+        int i;
+        for (i = 0; i < actors.size; i++) {
+            String tempActorName = actors.get(i).getName();
+            if (tempActorName != null && tempActorName.equals(life.getName())) {
+                actors.removeIndex(i);
+                break;
+            }
+        }
+
+        _stage.addActor(life);
     }
 
     @Override
