8ef1c720d172c0415d8288a9a360a810a50f21d5
ddedek <daniel.dedek@icloud.com>
2020-04-23 21:58:19 +0200

    Update maps/spawn

---
+++ core/assets/sprites/items/chests/chest_grass.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<
+tileset
+version = "1.2"
+tiledversion = "1.3.2"
+name = "chest_grass"
+tilewidth = "19"
+tileheight = "16"
+tilecount = "3"
+columns = "3" >
+    < image
+source = "chest_grass.png"
+width = "57"
+height = "16" / >
+    < / tileset >
